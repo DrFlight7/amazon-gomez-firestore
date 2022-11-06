4618423a248b9119a4d550bf450050ad69817f0a
Marco Rado <drflight7@gmail.com>
2022-11-06 16:56:28 +0800

    nextauth.js was added with secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ export default NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
+            secret: process.env.SECRET,
         }),
         FacebookProvider({
             clientId: process.env.GOOGLE_ID,
